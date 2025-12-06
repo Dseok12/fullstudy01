b85668776c012404200ced03502735a1a406accc
Dseok <ehskstkrjsa@naver.com>
2025-12-06 19:28:05 +0900

    11. (2.4) Shadcn/ui를 소개합니다

--- 00_winter/onebite-exam/src/App.tsx
+++ 00_winter/onebite-exam/src/App.tsx
@@ -5,9 +5,23 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-5xl font-bold underline'>
-      hello wrold
-    </div>
+    <>
+      {/* 1. 타이포그래피 */}
+      <div className='text-xs'>text-xs</div>
+      <div className='text-sm'>text-sm</div>
+      <div className='text-lg text-red-500 font-black'>text-lg</div>
+      <div className='text-xl'>text-xl</div>
+      <div className='text-2xl'>text-2xl</div>
+      <div className='text-3xl'>text-3xl</div>
+      <div className='text-[13px]'>text-13px</div>
+
+      {/* 2. 백그라운드컬러 */}
+      <div className='bg-amber-500'>bg-amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className='w-32 h-16 bg-sky-500 mt-[10px]'>w-32 h-16</div>
+      <div className='w-[200px] h-[200px] bg-sky-500 mt-[10px]'>w-[200px] h-[200px]</div>
+    </>
   )
 }
 
